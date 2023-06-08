2ebf271d16c11bfd74bb742ac34f272fabc8b5e0
Natalina686 <108237928+Natalina686@users.noreply.github.com>
2023-06-08 14:33:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,91 +16,96 @@
 
 // //   1 task
 
-// const list = [];
-// for (let i = 1; i <= 100; i++) {
-
-
-// if (i % 15 === 0) {
-//     list.push("FizzBuzz");
-//     }
-
-//     else if (i % 3 === 0) {
-//         list.push("Fizz");
-//         } else if (i % 5 === 0) {
-//         list.push("Buzz");
-//         }
-//         else {
-//             list.push(i);
-//             }
-//         }
-//             console.log(list);
-
 const screen = document.querySelector('.screen');
 
-function calculating(numbers) {
-  let result = [];
-
-  for (let number of numbers) {
-
-    if (number % 3 === 0 && number % 15 !== 0) {
-        result.push(`<p class="fizz"> Fizz </p>`)
-
-      } else if (number % 5 === 0 && number % 15 !== 0) {
-        result.push(`<p class="buzz"> Buzz </p>`);
-
-    } else if (number % 15 === 0) {
-      result.push(`<p class="fizzbuzz"> FizzBuzz </p>`);
-
-    } else {
-      result.push(`<p> ${number} </p>`)
-    }
-  }
-
-  screen.insertAdjacentHTML('beforeend',`${result}`);
-
+function posting(value) {
+  let el = `${value}`.toLowerCase();
+  return screen.insertAdjacentHTML('beforeend',`<p class="${el}"> ${value} </p>`);
 };
 
-// Iterator
+// iterator
 
-// let range = {
-//   from: 1,
-//   to: 100
-// }
-
-// range[Symbol.iterator] = function() {
-
-//   let current = this.from;
-//   let last = this.to;
-
-//   return {
-//     next() {
-//       if (current <= last) {
-//         return {
-//           done: false,
-//           value: current++
-//         };
-//       } else {
-//         return {
-//           done: true
-//         };
-//       }
-//     }
-//   }
-// };
-
-
-// calculating(range);
-
-// Generator
-
-function* generator(start, end) {
-
-  for (let i = start; i <= end; i++) {
-    yield i;
-  }
-
+let range = {
+  from: 1,
+  to: 100
 }
 
-let numberbersFromGenerator = [...generator(1, 100)];
+range[Symbol.iterator] = function() {
 
-calculating(numberbersFromGenerator);
+  let current = this.from;
+  let last = this.to;
+
+  return {
+    next() {
+      if (current <= last) {
+        let result = {
+          done: false,
+          value: ''
+        }
+        
+        if (current % 3 === 0 && current % 15 !== 0) {
+          result.value = 'Fizz';
+
+        } else if (current % 5 === 0 && current % 15 !== 0) {
+          result.value = 'Buzz';
+
+        } else if (current % 15 === 0) {
+          result.value = 'FizzBuzz';
+
+        } else {
+          result.value = current;
+        }
+        current++;
+        return result;
+      } else {
+        return {
+          done: true
+        }
+      }
+    }
+  }
+};
+
+for (let value of range) {
+  posting(value);
+}
+
+
+
+
+
+
+// generator
+// let numbersFromGenerator = generator(1, 100);
+// for (let number of numbersFromGenerator) {
+//   posting(number)
+// }
+
+// function* generator(start, end) {
+
+//   for (let i = start; i <= end; i++) {
+
+//     if (i % 3 === 0 && i % 15 !== 0) {
+//       yield 'Fizz';
+
+//     }else if (i % 5 === 0 && i % 15 !== 0) {
+
+//       yield 'Buzz';
+  
+//     }else if (i % 15 === 0) {
+
+//       yield 'FizzBuzz';
+
+//     } else {
+
+//       yield i;
+//     }
+    
+//   }
+
+// }
+
+
+
+
+
